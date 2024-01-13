06e3e613fa5430693e2c423cd66f6d8743868e99
Ed Hemphill <edhemphill@gmail.com>
2024-01-12 23:12:36 -0600

    fix test clickhouse to use normal networking. did not work on mac

--- start-clickhouse.sh
+++ start-clickhouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run -d \
-    --network=host \
+    -p 9000:9000 -p 8123:8123 \
     --cap-add=SYS_NICE --cap-add=NET_ADMIN --cap-add=IPC_LOCK \
     -v $(realpath ./ch_data):/var/lib/clickhouse/ \
     -v $(realpath ./ch_logs):/var/log/clickhouse-server/ \
